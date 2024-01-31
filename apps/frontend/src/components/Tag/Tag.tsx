f7c2717f3ccacc519a6a26dff49bcc4d301b20da
Nagaraj Devendhiran <nagaraj.devendhiran@netcentric.biz>
2024-01-31 17:43:28 +0100

    NA - Changed px to rem and used clsx

--- apps/frontend/src/components/Tag/Tag.tsx
+++ apps/frontend/src/components/Tag/Tag.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import vector from '../../assets/icons/vector.svg';
+import clsx from 'clsx';
 
 interface TagProps {
   label: string;
@@ -9,18 +10,19 @@ interface TagProps {
 export const Tag = ({ label, isSelected }: TagProps) => {
     const [selected, setSelected] = React.useState<boolean>(isSelected || false);
 
-  const classes = 'rounded-[40px] font-gellix text-[20px] leading-5 p-[30px] '.concat(
-    selected === true ? 'flex bg-midnight-blue text-white' : 'text-midnight-blue border border-midnight-blue hover:bg-midnight-blue hover:text-white',
-  );
+  const commonClasses = 'rounded-[2.5rem] font-gellix text-[1.25rem] leading-5 p-[1.875rem]';
 
   const handleClick = ()=> {
     setSelected(!selected);
   }
 
   return (
-    <div className={classes} onClick={handleClick}>
+    <div className={clsx(commonClasses,{
+        'flex bg-midnight-blue text-white': selected === true,
+        'text-midnight-blue border border-midnight-blue hover:bg-midnight-blue hover:text-white': selected === false
+    })} onClick={handleClick}>
       {label}
-      {selected && <img className='pl-[18px]' src={vector} alt="logo" />}
+      {selected && <img className='pl-[1.125rem]' src={vector} alt="logo" />}
     </div>
   );
 };
